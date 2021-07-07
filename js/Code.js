608e35a189ae872b569616eb1a6b616d2b47fbd2
shauryaagarwal1997 <75524612+shauryaagarwal1997@users.noreply.github.com>
2021-07-07 10:16:52 +0100

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("N U F T O I C N", 720,150)
+    fill("lightblue")
+    text("Hint: Performs Tasks !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A D B T A E A S", 175,300)
+    fill("lightblue")
+    text("Hint: Stores Information !!", 135,320)
+    
+
+}
